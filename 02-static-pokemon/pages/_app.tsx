3c41426b268d0597d97bd6fc2957d41fa9b5b797
Haru <frankcito639@gmail.com>
2023-07-11 10:48:14 -0500

    Actualizando

--- 02-static-pokemon/pages/_app.tsx
+++ 02-static-pokemon/pages/_app.tsx
@@ -1,11 +1,16 @@
-import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react'
-import '@/styles/globals.css'
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '../themes';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={ darkTheme }>
       <Component {...pageProps} />
     </NextUIProvider>
   )
 }
+
+export default MyApp
